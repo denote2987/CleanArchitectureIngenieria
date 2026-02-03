d02640d41084b1224467be7bf11e6a6b73c8363d
raset <raset@DESKTOP-2TGGUJA>
2026-02-02 21:29:58 -0500

    almacen y existencia

---
+++ CleanArchitecture.Domain/Entities/Almacen.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CleanArchitecture.Domain.Entities
+{
+    public class Almacen
+    {
+        [Required, MaxLength(120)]
+        public string Nombre { get; set; } = default!;
+
+        [MaxLength(250)]
+        public string? Ubicacion { get; set; }
+    }
+}
