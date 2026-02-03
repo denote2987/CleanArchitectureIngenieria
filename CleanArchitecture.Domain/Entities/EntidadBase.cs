7b8e352e2a39c4e128d7a938e4db3703785aa5fb
raset <raset@DESKTOP-2TGGUJA>
2026-02-02 21:18:13 -0500

    Agregar archivos de proyecto.

---
+++ CleanArchitecture.Domain/Entities/EntidadBase.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace CleanArchitecture.Domain.Entities
+{
+    public class EntidadBase
+    {
+        [Key]
+        public int Id { get; set; }
+        public bool Activo { get; set; }
+        public bool Eliminado { get; set; }
+        public DateTime FechaDeCreacion { get; set; } = DateTime.Now;
+        // nullable = puede ser nulo
+        public DateTime? FechaDeActualizacion { get; set; }
+    }
+}
